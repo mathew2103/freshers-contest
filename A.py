6d5521deb6095f4c15e0df996f44c59ab572a770
Mathew George <69890484+mathew2103@users.noreply.github.com>
2024-09-28 21:02:08 +0530

    code cleanup

--- A.py
+++ A.py
@@ -7,13 +7,3 @@ elif a>b:
     print(1)
 else:
     print(0)
-
-for i in range(len(a)):
-    if a[i] < b[i]:
-        print(-1)
-        break
-    elif a[i] > b[i]:
-        print(1)
-        break 
-else:
-    print(0)
